c51c625505c486ac9d9d817dc0ded40c9481262f
mark.szente <lovuho0@gmail.com>
2023-05-23 16:10:35 +0200

    created dummy song editor screen

---
+++ app/src/main/java/com/example/balaika/ui/composable/editor/SongEditor.kt
@@ -0,0 +1,13 @@
+package com.example.balaika.ui.composable.editor
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.balaika.ui.theme.WoodyCrayonWhite
+
+@Composable
+fun SongEditor() {
+    Text(
+        text = "Hello Balaika Editor",
+        color = WoodyCrayonWhite
+    )
+}
